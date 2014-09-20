939c139ca833229a04bbe227a089e9ff86a70d05
ati2 <math27182@gmail.com>
2014-09-20 08:15:01 -1000

    setting up

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+	<title>dexter</title>
+	<link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:300" rel="stylesheet" type="text/css">	
+	
+	<style> body{font-family: 'Source Sans Pro', sans serif; margin:30px;} 
+	</style>
+</head>
+<body>
+	<h1>Dexter</h1>
+	just a title page for now. 
+</body>
+</html>
