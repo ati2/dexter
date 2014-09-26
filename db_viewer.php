5b8c3379833c859edd525a724a906a07707c368b
ati2 <math27182@gmail.com>
2014-09-26 02:53:42 -1000

    dexter can take a directory of files, read, link, and visualize them

---
+++ db_viewer.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+	<title>db viewer</title>
+	<link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:300" rel="stylesheet" type="text/css">	
+	<link href="client_src/structure.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+	<h1>Dexter</h1>
+
+	<script src="client_src/js/d3.js"></script>
+	<script src="client_src/js/d3map.js"></script>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,13 +1,19 @@
+<!DOCTYPE>
 <html>
 <head>
 	<title>dexter</title>
 	<link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:300" rel="stylesheet" type="text/css">	
 	
-	<style> body{font-family: 'Source Sans Pro', sans serif; margin:30px;} 
-	</style>
+	<style> body{font-family: 'Source Sans Pro', sans serif; margin:30px;} </style>
 </head>
 <body>
 	<h1>Dexter</h1>
-	just a title page for now. 
+	<ul>
+		<a href="server/crawler.php"><li>this page runs through the file dir and calls the analyst</li></a>
+		<a href="server/analyst.php"><li>analyst reads 1 page and links it to the database<br>
+		(dont actually go to this one. crawler calls with and passes data into it)
+		</li></a>
+		<a href="db_viewer.php"><li>shows the state of the database</li></a>
+	</ul>
 </body>
 </html>
